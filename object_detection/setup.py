0a1a4bbabfe0f05cb0bce3a58209c9e619aea043
Dan Bernstein <danbernstein94@gmail.com>
2019-05-30 19:17:32 -0400

    Update setup.py

--- object_detection/setup.py
+++ object_detection/setup.py
@@ -7,7 +7,7 @@ setup(
     author='Dan Bernstein',
     author_email='danbernstein94@gmail.com',
     # Needed to actually package something
-    packages=['pdod','pdod.visuals','pdod.imports', 'pdod.analyses', 'pdod.detection', 'pdod.models'],
+    packages=['pdod'],
     # Needed for dependencies
     install_requires=[],
     # *strongly* suggested for sharing
