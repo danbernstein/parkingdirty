0ff0443bd0e1909e455b9325cc7f2f216174d4a6
Dan Bernstein <danbernstein94@gmail.com>
2019-05-30 20:28:42 -0400

    Update setup.py

--- object_detection/setup.py
+++ object_detection/setup.py
@@ -7,7 +7,7 @@ setup(
     author='Dan Bernstein',
     author_email='danbernstein94@gmail.com',
     # Needed to actually package something
-    packages=['pdod'],
+    packages=find_packages(),
     # Needed for dependencies
     install_requires=[],
     # *strongly* suggested for sharing
