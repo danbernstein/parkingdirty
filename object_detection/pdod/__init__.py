cf583be779a9591a4406ebd34dcbfd4dfa6a5900
Dan Bernstein <danbernstein94@gmail.com>
2019-06-01 17:18:36 -0400

    Update __init__.py

--- object_detection/pdod/__init__.py
+++ object_detection/pdod/__init__.py
@@ -1,1 +1,16 @@
+import os
 
+if not os.path.exists('object_detection/input_imgs'):
+    os.makedirs('object_detection/input_imgs')
+    
+    # download parking dirty images here when needed
+
+if not os.path.exists('object_detection/output_imgs'):
+    os.makedirs('object_detection/output_imgs')
+
+if not os.path.exists('object_detection/output_csv'):
+    os.makedirs('object_detection/output_csv')
+    
+    
+if not os.path.exists('object_detection/output_xml'):
+    os.makedirs('object_detection/output_xml/xml_files')
